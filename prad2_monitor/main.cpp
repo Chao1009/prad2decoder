7a9a2aa192830be42c134c5c14c81c06bc6171ad
Chao Peng <pengc2010@gmail.com>
2026-03-19 21:03:05 -0500

    fix a bug in the Qt monitor

--- prad2_monitor/main.cpp
+++ prad2_monitor/main.cpp
@@ -1,7 +1,9 @@
 #include <QApplication>
 #include <QWebEngineView>
+#include <QWebEnginePage>
 #include <QCommandLineParser>
 #include <QUrl>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -13,20 +15,34 @@ int main(int argc, char *argv[])
     parser.setApplicationDescription("Lightweight Qt WebEngine client for PRad2 event monitor");
     parser.addHelpOption();
     parser.addVersionOption();
-    parser.addPositionalArgument("url", "URL to load (default: http://localhost:8080)",
+    parser.addPositionalArgument("url", "URL to load (default: http://localhost:5050)",
                                 "[url]");
 
     parser.process(app);
 
-    QString url = "http://localhost:8080";
+    QString url = "http://localhost:5050";
     const QStringList args = parser.positionalArguments();
     if (!args.isEmpty()) {
         url = args.first();
     }
 
     QWebEngineView view;
-    view.setWindowTitle("PRad2 Monitor");
+    view.setWindowTitle("PRad2 Monitor - " + url);
     view.resize(1280, 800);
+
+    // show connection error instead of blank white page
+    QObject::connect(view.page(), &QWebEnginePage::loadFinished,
+                     [&view, &url](bool ok) {
+        if (!ok) {
+            view.setHtml(QString(
+                "<html><body style='font-family:sans-serif;padding:40px;color:#555;'>"
+                "<h2>Could not connect</h2>"
+                "<p>Failed to load <b>%1</b></p>"
+                "<p>Make sure the server (evc_viewer or evc_monitor) is running.</p>"
+                "</body></html>").arg(url));
+        }
+    });
+
     view.load(QUrl(url));
     view.show();
 
